b73802c706d8aaffc1faa5076bf3e038c368a1c8
Hussain000001 <mir.mh121@gmail.com>
2024-10-15 15:55:39 +0530

    modified scene.py

--- scene.py
+++ scene.py
@@ -6,6 +6,3 @@ class CreateCircle(Scene):
         circle = Circle()  # create a circle
         circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
         self.play(Create(circle))  # show the circle on screen
-
-if __name__ == "__main__":
-    print("hello world")
